619f30e2feb532f94a5b0a9e2ad50f7a6ed6b491
ClownZer0o <rezaif79.ri@gmail.com>
2021-07-15 17:01:21 +0700

    Configutation for server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
 const hapi = require('@hapi/hapi');
+
+const init = async function() {
+    let server = hapi.server({
+        port: 8000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes:{
+            cors:{
+                origin: ['*'],
+            }
+        }
+    })
+
+    server.routes();
+    await server.start();
+
+    console.log(`Server running on ${server.info.uri}`);
+}
