406e955d3a54998dea71edfd461a77857ca84b6d
ClownZer0o <rezaif79.ri@gmail.com>
2021-07-16 16:41:38 +0700

    Adding handler for editBookByIdHander on handler.js

--- src/handler.js
+++ src/handler.js
@@ -102,6 +102,64 @@ function getBookByIdHandler(request, h){
     return response;
 }
 
+function editBookByIdHandler(request, h){
+    const {bookId} = request.params;
+    
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = books.findIndex((book) => book.id === bookId);
+
+    if(name === ""){
+        const response = h.response({
+            status: "fail",
+            message: "Gagal memperbarui buku. Mohon isi nama buku",
+        });
+
+        response.code(400);
+        return response;
+
+    }else if(readPage > pageCount){
+        const response = h.response({
+            status: "fail",
+            message: "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+        });
+
+        response.code(400);
+        return response;
+
+    }else if(index !== -1){
+        books[index] = {
+            ...books[index],
+            name, 
+            year, 
+            author, 
+            summary, 
+            publisher, 
+            pageCount, 
+            readPage, 
+            reading,            
+        }
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui',
+        });
+    
+        response.code(200);
+        return response;
+    }else{
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Id tidak ditemukan',
+        });
+    
+        response.code(404);
+        return response;
+    }
+
+    
+}
+
 
 
 module.exports = { getAllBooksHandler, addBookHandler, getBookByIdHandler };
